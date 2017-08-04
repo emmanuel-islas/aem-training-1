cf1fe776c7b5919a87183be434301cf47d67f6d7
Emmanuel <emmanuel.islas@globant.com>
2017-08-04 12:20:26 -0500

    Fixing bug to display video

--- ui.apps/src/main/content/jcr_root/apps/w3/components/youtube-video/video-properties.js
+++ ui.apps/src/main/content/jcr_root/apps/w3/components/youtube-video/video-properties.js
@@ -1,17 +1,19 @@
 'use strict';
 
 use(function() {
-    var params = {
-        video_id: granite.resource.properties["url-video"],
-        provider: granite.resource.properties["platform"],
-        size: granite.resource.properties["type"]
-    };
+    var params = {};
     var urlProvider = '';
-    if(params.provider == '1') {
-        params.urlVideo = 'https://www.youtube.com/embed/' + params.video_id;
-    } else {
-       params.urlVideo = 'https://player.vimeo.com/video/' + params.video_id;
-    }
 
+    if(granite.resource.properties["urlVideo"]) {
+      params.video_id = granite.resource.properties["urlVideo"];
+      params.provider = granite.resource.properties["platform"];
+      params.size = granite.resource.properties["type"];
+
+      if(params.provider == '1') {
+          params.urlVideo = 'https://www.youtube.com/embed/' + params.video_id;
+      } else {
+         params.urlVideo = 'https://player.vimeo.com/video/' + params.video_id;
+      }
+    }
     return params;
 });
